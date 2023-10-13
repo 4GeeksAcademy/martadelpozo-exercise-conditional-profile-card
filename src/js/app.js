3b885c948d5bd7e4a12e33a822d19a7fd5c77256
martadelpozo <138892528+martadelpozo@users.noreply.github.com>
2023-10-13 19:46:08 +0000

    Addedd data to default values

--- src/js/app.js
+++ src/js/app.js
@@ -34,22 +34,20 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name || "John"}${variables.lastName || "Doe"}</h1>
+          <h1>${variables.name || "Marta"} ${variables.lastName ||
+    "del Pozo"}</h1>          
           <h2>${variables.role || "Developer"}</h2>
-          <h3>${variables.country || "USA, NY"}</h3>
+          <h3>${variables.country || "USA"}, ${variables.city || "NY"}	</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/${
-              variables.twitter
-            }"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${
-              variables.github
-            }""><i class="fab fa-github"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter ||
+              "4geeksacademyES"}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ||
+              "4GeeksAcademy"}""><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/${
               variables.linkedin
             }""><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${
-              variables.instagram
-            }"y"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ||
+              "4geeksacademy"}"y"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
